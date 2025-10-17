7481138cb9973c6d141c83031077a3a480496549
Nitya Arya <nitya.4r@gmail.com>
2025-10-17 06:16:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -679,7 +679,7 @@ if mode == "Dashboard":
                 # Muted, modern palette
                 screen_domain = keep
                 screen_range = [
-                    "#2F1E3A",  # deep plum
+                    "#00BFA6",  # tiffany teal
                     "#D07B62",  # terracotta clay
                     "#EDE7DE",  # bone/porcelain
                     "#B9A07A",  # desert taupe
