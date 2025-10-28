a6a6f7ed6328156079649e088a68c145478f6ac4
Nitya Arya <nitya.4r@gmail.com>
2025-10-29 03:54:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -806,7 +806,7 @@ def render_change_since_2017():
                 "#B5651D",  # Deforestation (rose/pink)
                 "#800020",  # Fossil Fuel (orange)
                 "#F08BA6",  # Weapons (very dark red/brown)
-                "#8B2C2C",  # Tobacco (brown)
+                "#D2681E",  # Tobacco (brown)
             ]
             H = 300
             y_dom_scr = _dyn_domain_zero(d["value"])
